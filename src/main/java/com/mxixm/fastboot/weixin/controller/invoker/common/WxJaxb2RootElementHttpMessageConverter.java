e3c654ff5c4eb8411b1bfabac62d2687a27e84b4
Guangshan <guangshan1992@qq.com>
2017-09-07 00:28:52 +0800

    maven中央仓库通过审核，准备发布

--- src/main/java/com/mxixm/fastboot/weixin/controller/invoker/common/WxJaxb2RootElementHttpMessageConverter.java
+++ src/main/java/com/mxixm/fastboot/weixin/controller/invoker/common/WxJaxb2RootElementHttpMessageConverter.java
@@ -14,6 +14,11 @@ import java.lang.invoke.MethodHandles;
 /**
  * FastBootWeixin  WxJaxb2RootElementHttpMessageConverter
  * 用于转化CData，暂时不使用，实在没办法了再使用
+ * 这里其实还引出了一个大问题，因为我这里使用了CharacterEscapeHandler接口，这个是在rt.jar包中的
+ * com.sun.xml.internal.bind.marshaller.CharacterEscapeHandler，因为是rt.jar，所以在编译时并不会把这个包加进去
+ * 所以一直编译错误，强行加入编译成功了，但是这种方式是不推荐使用的，推荐替换成其他公用实现，而不是rt.jar中sun开头的私有实现
+ * 如maven仓库中的 jaxb-impl-2.3.0.jar，就是这个的相同实现，包名中去掉internal即可。
+ * 如果有一天真的要启用这个类，那么可以参考spring的ConditionOnClass来判断两种实现是否存在，如果存在则使用。
  *
  * @author Guangshan
  * @summary FastBootWeixin  WxJaxb2RootElementHttpMessageConverter
